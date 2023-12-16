b040e0dac0905b10eb51663cfbe790e89cbd3e49
Sharayah <95879449+sereyes17@users.noreply.github.com>
2023-12-16 14:29:01 -0500

    Add files via upload

---
+++ madLib.py
@@ -0,0 +1,12 @@
+__author__ = 'Sharayah Reyes'
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famousPerson = input("Famous Person: ")
+
+madLib = f"Computer programming is so {adj}! It makes me so excited all the time \
+because I love to {verb1}. Stay hydrated and {verb2} like you are {famousPerson}!"
+
+print(madLib)
+
